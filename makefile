eb21bc4986c4ffad6c4cd08943fa8c46c72067f1
Jeremy Siegelstein <Jeremy@Jeremys-MacBook-Air.local>
2020-02-28 13:45:01 -0500

    Pushing for linux

--- makefile
+++ makefile
@@ -7,10 +7,11 @@ udp_server.o: udp_server.cpp
 	g++ -Wall -Wextra -pedantic -std=c++17 -c udp_server.cpp
 
 client:	udp_client.o
-    g++ udp_client.o -o client
+	g++ udp_client.o -o client
 
 udp_client.o: udp_client.cpp
-    g++ -Wall -Wextra -pedantic -std=c++17 -c udp_client.cpp
+	g++ -Wall -Wextra -pedantic -std=c++17 -c udp_client.cpp
+
 clean:
 	-rm -f *.o server
-    -rm -f *.o client
+	-rm -f *.o client
--- udp_client.cpp
+++ udp_client.cpp
@@ -26,13 +26,16 @@ int main(){
 
     // Fill server information
     servaddr.sin_family = AF_INET; // IPv4
-    //servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
+    servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
     servaddr.sin_port = htons(PORT); // port number
 
     clock_t start, end;
     double duration;
     //timeout will be measured in milliseconds, so 1000ms = 1s
     static int timeout = 1000;
+
+    //Set timeout to 1 second
+    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&timeout,sizeof(timeout));
 
     for(int i = 0; i < 10; i++){
 
@@ -45,15 +48,12 @@ int main(){
         //Send packet to server
         n = sendto(sockfd, (char *) buffer, strlen(buffer), MSG_CONFIRM, (struct sockaddr *) &servaddr, &len)
 
-        //Set timeout to 1 second
-        setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,(char*)&timeout,sizeof(timeout));
-
         //Receive response from server
         //If the packet is not received in one second, time out and the function returns -1
         //If recvfrom returns -1, we assume the packet was lost
         //Otherwise, the packet was received so we track the round trip time
         if (recvfrom(sockfd, (char *)buffer, sizeof(buffer), MSG_WAITALL, ( struct sockaddr *) &servaddr, &len) < 0){
-            cout << "Packet " << i << " was lost." << endl;
+            std::cout << "Packet " << i << " was lost." << std::endl;
         }
         else{
             //get end time
@@ -62,7 +62,7 @@ int main(){
             //calculate duration
             duration = (double) (end - start)/CLOCKS_PER_SEC;
 
-            cout << "Round trip time of packet " << i << " is " << duration << " seconds." << endl;
+            std::cout << "Round trip time of packet " << i << " is " << duration << " seconds." << std::endl;
         }
     }
     //close the socket
